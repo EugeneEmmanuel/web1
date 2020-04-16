f39f8d14dd614d6b9541a9ad63ad8c463fb4779e
EugèneEmmanuël Dubuc <eugene-emmanuel@hotmail.com>
2020-04-15 21:09:40 -0400

    Fix issues with the music

--- ResidentEvilEscape/javascript/index.js
+++ ResidentEvilEscape/javascript/index.js
@@ -2,7 +2,7 @@
 
 let lastSong = null;
 let selection = null;
-let playlist = ["audio/re1_original_saveroom.mp3", "audio/re1_remake_saveroom.mp3", "audio/re2_original_saveroom.mp3", "audio/re3_original_saveroom.mp3"]; // Liste des musiques
+let playlist = ["audio/re1_original_saveroom.mp3", "audio/re1_remake_saveroom.mp3", "audio/re3_original_saveroom.mp3", "audio/re2_original_saveroom.mp3"]; // Liste des musiques
 let player = document.getElementById("audioplayer"); // Obtenir l'audio par son ID
 player.autoplay=true;
 player.addEventListener("ended", selectRandom); // Appel la fonction random quand la chanson se termine
